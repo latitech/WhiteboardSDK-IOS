d7c755657bb2326a0c2e0d35126eb98841240cff
yanqing.zhang <76620790@qq.com>
2021-06-26 17:34:46 +0800

    fix bug:wrong screen size after open camera

--- DemoProject/TestNavigatorController.m
+++ DemoProject/TestNavigatorController.m
@@ -8,19 +8,39 @@
 #import "TestNavigatorController.h"
 #import "DemoViewController.h"
 
-#if 1
+#if 0
 #define TOKEN_FIRST @"127182d9a297292558aa3d935746827c";
 #define APP_ID @"a4b26ecae3744e3fb60ff679e186cd98";
 #define ROOM_ID_FIRST @"0f9ce36096bb4330bdee6ca9acd6b89f";
 #define USER_ID_FIRST @"07345de7-5a80-48d4-86fd-d75635525d7d";
-#else
-#define APP_ID @"fd3c029d48a64afeb5b46c24aedc2995"
-#define USER_ID_FIRST @"test1"
-#define ROOM_ID_FIRST @"aede5dfa1c194803b40799783b5d92c2"
-#define TOKEN_FIRST @"63e511d858589eab6d9e8a4d8029a1c0"
-#endif
+/*
+#elif _ANDROID
+#define TOKEN_FIRST @"82abb8d203e2fe01a749655237eea0df";
+#define APP_ID @"a4b26ecae3744e3fb60ff679e186cd98";
+#define ROOM_ID_FIRST @"4ca05ca0cb2b4fc38f6f655989a5616f";
 
-#define USER_ID_SECOND  @"0585184c-b515-4533-b146-1d39379e7d9a"
+#define USER_ID_FIRST @"0457987b-82f7-409d-8d58-24b42dda76f2";
+ */
+#else
+
+
+#define APP_ID @"a4b26ecae3744e3fb60ff679e186cd98"
+#define USER_ID_FIRST @"test"
+#define ROOM_ID_FIRST @"ae4a29b6280e4c759fb2160cc6f5e3ec"
+#define TOKEN_FIRST @"d024cbe961938bf2bb6e56355a96d2c9"
+
+/*
+//from android
+#define APP_ID @"a4b26ecae3744e3fb60ff679e186cd98"
+
+#define ROOM_ID_FIRST @"399bbd57486b4a83b68485f353dd7153"
+
+#define USER_ID_FIRST @"test"
+
+#define TOKEN_FIRST @"df50f8ffdc59d7b40d2c97b9c7aa8a60"
+*/
+#endif
+#define USER_ID_SECOND  @"0585184cb5154533b1461d39379e7d9a"
 #define ROOM_ID_SECOND  @"95daddc650294ddda5ea42d84dffe838"
 #define TOKEN_SECOND  @"42e564342aa38b7be51bda37f5590f5d"
 
@@ -62,10 +82,10 @@
     DemoViewController * demoEntry = [board instantiateViewControllerWithIdentifier:@"demoEntry"];
     [demoEntry initConnectionInfo:appIdStr roomId:roomIdStr userId:userIdStr token:tokenStr];
     demoEntry.modalPresentationStyle = UIModalPresentationOverCurrentContext;
-//    [self.navigationController pushViewController:demoEntry animated:YES];
     [self.navigationController presentViewController:demoEntry animated:YES completion:^{
-        
+
     }];
+    
 }
 - (IBAction)roomChanged:(UISegmentedControl *)sender {
     if(sender.selectedSegmentIndex == 0)

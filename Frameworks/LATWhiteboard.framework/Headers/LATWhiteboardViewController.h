5a7268c87a5c764c4ef8009b81530b5c3a5e3301
macimay <macimay@gmail.com>
2021-10-21 15:49:28 +0800

    fix bug:transparent background is changed to green

--- Frameworks/LATWhiteboard.framework/Headers/LATWhiteboardViewController.h
+++ Frameworks/LATWhiteboard.framework/Headers/LATWhiteboardViewController.h
@@ -12,8 +12,42 @@
 #import "LATUIDelegate.h"
 
 NS_ASSUME_NONNULL_BEGIN
+typedef NS_ENUM(NSInteger,LATViewHorizonAlign)
+{
+    //水平方向居中对齐
+    LATViewHorizonAlignCenter,
+    //水平方向左对齐
+    LATViewHorizonAlignLeft,
+    //水平方向右对齐
+    LATViewHorizonAlignRight
+};
+typedef NS_ENUM(NSInteger,LATViewVerticalAlign)
+{
+    //垂直方向居中对齐
+    LATViewVerticalAlignCenter,
+    //垂直方向顶部对齐
+    LATViewVerticalAlignTop,
+    //垂直方向底部对齐
+    LATViewVerticalAlignBottom
+};
 
 @interface LATWhiteboardViewController : GLKViewController <GLKViewControllerDelegate,LATUIDelegate,LATWhiteboardDelegate>
+
+
+/**
+ lockRatio表示是否根据内容的比例来锁定屏幕，适配的规则由horAlign和verAlign来指定。默认需要适配。
+ */
+@property (nonatomic,assign) BOOL lockRatio;
+
+/**
+    view在autoScale为YES的情况下水平对齐设定，默认为居中
+ */
+@property (nonatomic,assign) LATViewHorizonAlign horAlign;
+
+/**
+    view在autoScale为YES的情况下垂直对齐设定，默认值为居中
+ */
+@property (nonatomic,assign) LATViewVerticalAlign verAlign;
 
 -(bool)initRender:(CGSize)size;
 
